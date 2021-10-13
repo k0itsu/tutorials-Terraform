9f10d5ce985ae168a41cb0b8656a062fa84766f0
Edward Delin <eldelin@gmail.com>
2021-10-13 01:16:31 -0400

    add data source to helloworld example

--- terraform-in-action/ch01/main.tf
+++ terraform-in-action/ch01/main.tf
@@ -2,8 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/uuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
 resource "aws_instance" "helloworld" {
-  ami           = "ami-02e136e904f3da870"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
