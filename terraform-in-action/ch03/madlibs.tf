725e4c1dc02bae1b82803e0f46428d4496d39417
Edward Delin <eldelin@gmail.com>
2021-10-13 22:32:28 -0400

    complete the madlibs example

--- terraform-in-action/ch03/madlibs.tf
+++ terraform-in-action/ch03/madlibs.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 0.15"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
 }
 
 variable "words" {
@@ -13,7 +19,40 @@ variable "words" {
   })
 
   validation {
-    condition     = length(var.words["nouns"]) >= 20
+    condition     = length(var.words["nouns"]) >= 5
     error_message = "At least 20 nouns must be supplied"
   }
+}
+
+resource "random_shuffle" "random_nouns" {
+  input = var.words["nouns"]
+}
+
+resource "random_shuffle" "random_adjectives" {
+  input = var.words["adjectives"]
+}
+
+resource "random_shuffle" "random_verbs" {
+  input = var.words["verbs"]
+}
+
+resource "random_shuffle" "random_adverbs" {
+  input = var.words["adverbs"]
+}
+
+resource "random_shuffle" "random_numbers" {
+  input = var.words["numbers"]
+}
+
+output "mad_libs" {
+  # path.module is a reference to the filesystem path of the
+  # containing module
+  value = templatefile("${path.module}/templates/alice.txt",
+    {
+      nouns      = random_shuffle.random_nouns.result
+      adjectives = random_shuffle.random_adjectives.result
+      verbs      = random_shuffle.random_verbs.result
+      adverbs    = random_shuffle.random_adverbs.result
+      numbers    = random_shuffle.random_numbers.result
+  })
 }
