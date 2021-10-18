18c306aae0ad2468ef23774bf8ae79ca7593d7e8
Edward Delin <eldelin@gmail.com>
2021-10-18 17:14:16 -0400

    add variables to pass data from the networking module

--- terraform-in-action/ch04/modules/networking/variables.tf
+++ terraform-in-action/ch04/modules/networking/variables.tf
@@ -1,4 +1,14 @@
+# These are the input variables for namespace, vpc, and sg
 variable "namespace" {
   type = string
 }
 
+variable "vpc" {
+  type = any
+  # type constraint of 'any' means Terraform will skip type checking
+  # try to use this only for passing data between modules
+}
+
+variable "sg" {
+  type = any
+}
