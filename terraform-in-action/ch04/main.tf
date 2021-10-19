8959e046acf5818b96139be297b48285e6450673
Edward Delin <eldelin@gmail.com>
2021-10-18 21:42:59 -0400

    trickle data down into the autoscaling module

--- terraform-in-action/ch04/main.tf
+++ terraform-in-action/ch04/main.tf
@@ -1,6 +1,12 @@
 module "autoscaling" {
-  source    = "./modules/autoscaling"
-  namespace = var.namespace
+  source      = "./modules/autoscaling"
+  namespace   = var.namespace
+  
+  # input args for the autoscaling module, set by other module's outputs
+  ssh_keypair = var.ssh_keypair
+  vpc         = module.networking.vpc
+  sg          = module.networking.sg
+  db_config   = module.database.db_config
 }
 
 module "database" {
