dfa38cd4628d7589f1d8729f1bf917f13bd74934
Edward Delin <eldelin@gmail.com>
2021-10-19 23:33:31 -0400

    add random_string resource to generate unique resource group names

--- terraform-in-action/ch05/main.tf
+++ terraform-in-action/ch05/main.tf
@@ -2,3 +2,13 @@ resource "azurerm_resource_group" "default" {
   name     = local.namespace
   location = var.location
 }
+
+resource "random_string" "rand" {
+  length  = 24
+  special = false
+  upper   = false
+}
+
+locals {
+  namespace = substr(join("-", [var.namespace, random_string.rand.result]), 0, 24)
+}
