f466505e237a9c41347e7d6c51938b28e84710c6
Edward Delin <eldelin@gmail.com>
2021-10-20 13:06:29 -0400

    add datasource to produce sas token for azure functions to download application code

--- terraform-in-action/ch05/main.tf
+++ terraform-in-action/ch05/main.tf
@@ -48,3 +48,37 @@ resource "azurerm_storage_blob" "storage_blob" {
   type                   = "Block"
   source                 = module.ballroom.output_path
 }
+
+# step 4 of 4 - function app
+# use a data source to produce a shared access signature (SAS) token 
+# to be able to download the application source code from the storage blob
+data "azurerm_storage_account_sas" "storage_sas" {
+  connection_string = azurerm_storage_account.storage_account.primary_connection_string
+
+  resource_types {
+    service   = false
+    container = false
+    object    = true
+  }
+
+  services {
+    blob      = true
+    queue     = false
+    table     = false
+    file      = false
+  }
+
+  start       = "2021-10-20T00:00:00Z"
+  expiry      = "2022-10-20T00:00:00Z"
+
+  permissions {
+    read      = true
+    write     = false
+    delete    = false
+    list      = false
+    add       = false
+    create    = false
+    update    = false
+    process   = false
+  }
+}
