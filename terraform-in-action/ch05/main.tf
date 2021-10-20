593d02f9f532ba0199facc8e850726fe803a2704
Edward Delin <eldelin@gmail.com>
2021-10-20 12:34:42 -0400

    add build artifact and storage blob step

--- terraform-in-action/ch05/main.tf
+++ terraform-in-action/ch05/main.tf
@@ -17,7 +17,8 @@ locals {
 }
 
 # step 2 of 4 -
-# storage container for azure - to store application source code and documents in a nosql db
+# storage account provides metadata about where the data will be stored
+# and how much redundancy/data replication you want
 resource "azurerm_storage_account" "storage_account" {
   name                     = random_string.rand.result
   resource_group_name      = azurerm_resource_group.default.name
@@ -26,9 +27,24 @@ resource "azurerm_storage_account" "storage_account" {
   account_replication_type = "LRS"
 }
 
+# storage container for azure - to store application source code and documents in a nosql db
 resource "azurerm_storage_container" "storage_container" {
   name                  = "serverless"
   storage_acccount_name = azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
 
+# step 3 of 4 - storage blob
+# this is where we insert the application code
+# the example uses a terraform module 'shim' to get the application code
+module "ballroom" {
+  source = "terraform-in-action/ballroom/azure"
+}
+
+resource "azurerm_storage_blob" "storage_blob" {
+  name                   = "server.zip"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.storage_container.name
+  type                   = "Block"
+  source                 = module.ballroom.output_path
+}
