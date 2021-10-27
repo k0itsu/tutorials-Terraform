f518af13a677ac3deb65cf2e32b71b7424b211b0
Edward Delin <eldelin@gmail.com>
2021-10-26 21:09:37 -0400

    use dynamic blocks for codebuild instead of statically declared build steps

--- terraform-in-action/ch07/gcp-pipelines/infrastructure/main.tf
+++ terraform-in-action/ch07/gcp-pipelines/infrastructure/main.tf
@@ -37,6 +37,31 @@ resource "google_sourcerepo_repository" "repo" {
   name = "${var.namespace}-repo"
 }
 
+locals {
+  image = "gcr.io/${var.project_id}/${var.namespace}"
+  steps = [
+    {
+      name = "gcr.io/cloud-builders/go"
+      args = ["test"]
+      env  = ["PROJECT_ROOT=${var.namespace}"]
+    },
+    {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["build", "-t", local.image, "."]
+    },
+    {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["push", local.image]
+    },
+    {
+      name = "gcr.io/cloud-builders/gcloud"
+      args = ["run", "deploy", google_cloud_run_service.service.name,
+      "--image", local.image, "--region", var.region, "--platform",
+      "managed", "-q"]
+    }
+  ]
+}
+
 resource "google_cloudbuild_trigger" "trigger" {
   depends_on = [
     google_project_service.enabled_service["cloudbuild.googleapis.com"]
@@ -48,26 +73,15 @@ resource "google_cloudbuild_trigger" "trigger" {
   }
 
   build {
-    step {
-      name = "gcr.io/cloud-builders/go"
-      args = ["test"]
-      env  = ["PROJECT_ROOT=${var.namespace}"]
-    }
-
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = ["build", "-t", local.image, "."]
-    }
-
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = ["push", local.image]
-    }
-
-    step {
-      name = "gcr.io/cloud-builders/gcloud"
-      args = ["run", "deploy", google_cloud_run_service.servicename, "--image", local.image,
-      "--region", var.region, "--platform", "managed", "-q"]
+    dynamic "step" {
+      for_each = local.steps
+      content {
+        name = step.value.name
+        args = step.value.args
+        # not all steps have 'env' set. lookup() returns null if
+        # step.value["env"] is not set.
+        env  = lookup(step.value, "env", null)
+      }
     }
   }
 }
