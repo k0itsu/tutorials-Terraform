bc15be632919a0bfc459400e6314b04bdb1d89e4
Edward Delin <eldelin@gmail.com>
2021-10-26 20:47:32 -0400

    add first stage of cicd pipeline: provision version-controlled source repo

--- terraform-in-action/ch07/gcp-pipelines/infrastructure/main.tf
+++ terraform-in-action/ch07/gcp-pipelines/infrastructure/main.tf
@@ -12,14 +12,27 @@ resource "google_project_service" "enabled_service" {
   project  = var.project_id
   service  = each.key
 
-  # Creation-time provisioner
+  # these pauses are here to avoid potential race conditions when
+  # enabling/disabling service APIs
+
+  # Creation-time provisioner - pause 60 seconds after Create()
   provisioner "local-exec" {
+    # if you don't specify 'when' it defaults to apply
+    # when    = apply
     command = "sleep 60"
   }
 
-  # Destruction-time provisioner
+  # Destruction-time provisioner - pause 15 seconds before Delete()
   provisioner "local-exec" {
     when    = destroy
     command = "sleep 15"
   }
+}
+
+resource "google_sourcerepo_repository" "repo" {
+  depends_on = [
+    google_project_service.enabled_service["sourcerepo.googleapis.com"]
+  ]
+
+  name = "${var.namespace}-repo"
 }
