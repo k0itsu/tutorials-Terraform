00faf0c92e86b74e221d9bf5c10908ac2d116352
Edward Delin <eldelin@gmail.com>
2021-11-01 15:58:52 -0400

    building the plan and apply stages

--- terraform-in-action/ch12-01/main.tf
+++ terraform-in-action/ch12-01/main.tf
@@ -1,6 +1,15 @@
+resource "random_string" "rand" {
+  length  = 24
+  special = false
+  upper   = false
+}
+
+locals {
+  namespace = substr(join("-", [var.name, random_string.rand.result]), 0, 24)
+}
+
 # create a new Terraform workspace and declare the s3backend
 # and codepipeline modules
-
 variable "vcs_repo" {
   type = object({ identifier = string, branch = string })
 }
@@ -33,4 +42,31 @@ module "codepipeline" {
   # deployment_policy file not created yet in first commit
   deployment_policy = file("./policies/helloworld.json")
   s3_backend_config = module.s3backend.config
+}
+
+
+## provision the two aws codebuild projects
+locals {
+  projects = ["plan", "apply"]
+}
+
+resource "aws_codebuild_project" "project" {
+  count        = length(local.projects)
+  name         = "${local.namespace}-${local.projects[count.index]}"
+  service_role = aws_iam_role.codebuild.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "hashicorp/terraform:${var.terraform_version}"
+    type         = "LINUX_CONTAINER"
+  }
+
+  source {
+    type      = "NO_SOURCE"
+    buildspec = file("${path.module}/templates/buildspec_${local.projects[count.index]}.yml")
+  }
 }
